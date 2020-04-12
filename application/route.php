037cb9e0ba95cf36d0db7db2840563f960fdebc9
huabowen <1041893894@qq.com>
2020-04-12 12:06:18 +0800

    sql配置

--- application/route.php
+++ application/route.php
@@ -8,14 +8,10 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+use think\Route;
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
+//配置后台模块/域名
+Route::domain('adminapi',function (){
+    Route::get('/','adminapi/index/index');
+});
 
-];
